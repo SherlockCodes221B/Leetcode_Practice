67810989c11c8f7f2b1e21cafc5037d2ffaca8c1
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-08 17:29:07 +0530

    8/7/2024

---
+++ Leetcode150/merge_sorted.py
@@ -0,0 +1,22 @@
+'''
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+
+'''
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m
+        p2 = 0
+        while p1 < len(nums1):
+            nums1[p1] = nums2[p2]
+            p1 = p1 + 1
+            p2 = p2 + 1
+        nums1.sort()
