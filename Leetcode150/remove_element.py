ec274312f810477a7b0494641072f500f6b9a426
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-10 15:14:55 +0530

    10/7/2024 - duplicates are tricky

---
+++ Leetcode150/remove_element.py
@@ -0,0 +1,20 @@
+'''
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+'''
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index = index + 1
+        return index
