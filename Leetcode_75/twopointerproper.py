e7fd13d676a57fadf91b514da8e8d9eab9447ed2
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-24 15:27:59 +0530

    24/07/2024

--- Leetcode_75/twopointerproper.py
+++ Leetcode_75/twopointerproper.py
@@ -11,3 +11,21 @@ nums = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
 k = 2
 o/p: 2
 '''
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i = 0 
+        c = 0
+        j = len(nums)-1
+        while i < j:
+            if nums[i] + nums[j] == k:
+                c = c + 1
+                i = i + 1
+                j = j - 1
+            elif nums[i] + nums[j] > k:
+                j = j - 1
+            else:
+                i = i + 1
+        return c
