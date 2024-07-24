206b74151a0b3aa9402382435a126449dfe3cc6d
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-24 15:27:34 +0530

    24/07/2024

---
+++ Leetcode_75/twopointerproper.py
@@ -0,0 +1,13 @@
+'''
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+nums = [4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4]
+k = 2
+o/p: 2
+'''
