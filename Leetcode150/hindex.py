b0ec2b79da91a350cf768c308b5ea121e3f70665
Sherlock Holmes <shreeshastudy@gmail.com>
2024-09-17 10:38:48 +0530

    17/9/2024

---
+++ Leetcode150/hindex.py
@@ -0,0 +1,18 @@
+'''
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+Still wondering about the question tho
+'''
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+
+        for i,v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
